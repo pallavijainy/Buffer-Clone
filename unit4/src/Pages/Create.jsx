94c16f33a707e77ae27c8746ecc22b592791658f
pallavi jain <pallavijainy0612@gmail.com>
2022-11-12 19:06:31 +0530

    add

--- unit4/src/Pages/Create.jsx
+++ unit4/src/Pages/Create.jsx
@@ -1,33 +1,63 @@
 import React, { useState } from 'react'
-import {Box, Button, Checkbox, Flex, FormControl, FormLabel, Heading, Image, Input, Stack, Text} from "@chakra-ui/react"
+import {Avatar, Box, Button, Checkbox, Flex, FormControl, FormLabel, Heading, Image, Input, Stack, Tag, Text, WrapItem} from "@chakra-ui/react"
 import Logo from "../component/Image/PostAdda.png"
 import {Link} from "react-router-dom";
 import axios from "axios";
+import {Navigate} from "react-router-dom";
 
 export const Create = () => {
 
   const [email,setEmail]=useState("")
   const [password,setPassword]=useState("")
+const [status,setStatus]=React.useState(false);
+ const [token,setToken]=React.useState("")
 
-const handleClick=()=>{
- axios.post(`https://reqres.in/api/register`,{
-   email:email,password:password
- }).then((res)=>console.log(res.data)).catch((err)=>console.log(err))
-}
+const getData={
+   
+  email:email,
+  password:password,
+  
+ 
+ }
+
+ const handleClick=()=>{
+      return axios({
+          method:"POST",
+            
+            url:`http://localhost:3000/posts`,
+          data:getData
+      }).then((res)=>{
+  console.log(res)
+  setToken(Date.now())
+  alert("Sign up successfull")
+  setStatus(true)
+      })
+      .catch((err)=>{
+          setStatus(false)
+          console.log(err)
+          console.log("something went wrong")
+      })
+  }
+  
+  if(status===true)
+     {
+     return <Navigate to="/login"/>
+     }
+  
 
 
   return (
     <>
-    <Box display={"flex"}>
+    <Box display={{lg:"flex"}}>
       <Box width={"60%"} margin={"auto"} >
-      <Flex flex={{ base: 1 }} justify={{ base: 'center', md: 'start' }} >
+      <Flex flex={{ base: 1 }} justify={{md: 'start' }} >
         <Image  boxSize="150px" src={Logo} alt='logo'/>
 
         <Heading as='h1' size='2xl'><Link to={"/"} >Post</Link></Heading>
     <Image  boxSize="50px" src={Logo} alt='logo'/>
 <Heading size='2xl'>dda</Heading>
 </Flex>
-      <FormControl width={"50%"} margin={"auto"} >
+      <FormControl width={{sm:"80%",md:"80%",lg:"55%"}} margin={"auto"} >
         <Stack>
         <Heading textAlign={"left"} >Let's get your account set up</Heading>
   <FormLabel>Email</FormLabel>
@@ -46,13 +76,44 @@ const handleClick=()=>{
   <Button colorScheme='blue' variant='outline' onClick={handleClick}>
    Sign Up
   </Button>
+  <Box display={{sm:"grid",md:"grid",lg:"flex"}}>
+  <Tag bg="transparent" m={5}>I agree to Buffer's Terms of Service</Tag>
+  <Tag bg="transparent" m={5}>Already have an account?</Tag>
+
+  </Box>
+ 
   </Stack>
 </FormControl>
       </Box>
-      <Box backgroundImage={"https://static.buffer.com/login/public/img/signup-bkg-publish.png"} height={"700px"}width={"40%"}>
-         
+      <Box backgroundImage={"https://static.buffer.com/login/public/img/signup-bkg-publish.png"} height={"700px"}width={"40%"} display={{base:"none",sm:"none",md:"none",lg:"block"}}>
+        <Box mt={"200px"} width={"500px"} ml={20}>
+        <Heading as='h2' size='lg' color={"white"} >
+      “With Buffer I can build social content out as far as I want into the future but also tailor campaigns if we see certain trends within the industry.”
+  </Heading>
+
+        </Box>
+      
+  <Box mt={10}>
+  <WrapItem>
+    <Avatar size='xl'
+    ml={60}
+      name='Prosper Otemuyiwa'
+      src='https://static.buffer.com/login/public/img/signup-avatar-publish.png'
+    />{' '}
+  </WrapItem>
+
+  <Heading as='h5' size='sm'>
+  Justin Ozanich
+
+
+  </Heading>
+  <Text fontSize='md'>Marketing Manager</Text>
+  <Text fontSize='md'>Foster Coffee Company</Text>
+  </Box>
       </Box>
     </Box>
     </>
   )
 }
+
+
--- unit4/src/Pages/Login.jsx
+++ unit4/src/Pages/Login.jsx
@@ -2,14 +2,52 @@ import { Box, Button, Checkbox, FormLabel, Image, Input, Text } from '@chakra-ui
 import React, { useState } from 'react'
 import axios from 'axios'
 
+import {Navigate,Link} from "react-router-dom";
+
+
 export const Login = () => {
 
   const [email,setEmail]=useState("")
   const [password,setPassword]=useState("")
+const [status,setStatus]=React.useState(false);
 
-const handleClick=()=>{
- 
+const getData={
+  email:email,
+  password:password
+ }
+ const handleLogin=()=>{
+  return axios({
+      method:"GET",
+        
+      url:`http://localhost:3000/posts`,
+      data:getData
+  }).then((res)=>{
+   checkcredentials(res.data)
+
+})
+
 }
+
+const checkcredentials=(data)=>{
+  let filtered=data.filter((el)=>{
+    return el.email===email && el.password===password
+  })
+  return finalcheck(filtered)
+}
+ 
+const finalcheck=(filtered)=>{
+  if(filtered.length>0){
+  alert("login successfull")
+ setStatus(true)
+   } else{
+  alert("login failed")
+   }
+}
+if(status===true)
+   {
+    return <Navigate to="/"/>
+   }
+
 
 
 
@@ -29,11 +67,53 @@ const handleClick=()=>{
   </Box>
   
   </Box>
-  <Button colorScheme='blue' variant='outline' onClick={handleClick}>
-   Sign Up
-  </Button>
+  <Input onClick={handleLogin} m={5} bg="black" color={"white"} 
+    type="submit" value="Log in"/>
 
 
-    </div>
-  )
-}
+  </div>
+)
+ }
+
+
+
+
+
+
+
+
+
+
+  
+
+
+
+
+
+
+    
+
+
+
+
+//   </GridItem>
+//   <GridItem colStart={3} colEnd={6} h='10' w="400px">
+// <Heading>REGISTER</Heading>
+// <Tag bg="transparent" m={5}>If you still don't have a Zara.com account, use this option to access the registration form.</Tag>
+// <Tag bg="transparent" ml="25px">By giving us your details, purchasing in Zara.com will be faster and an enjoyable experience.</Tag>
+// <Link to="/register">
+// <Input w="300px" mt={100} bg="black" color={"white"} type="submit" value="REGISTER"/>
+// </Link>
+
+//   </GridItem>
+// </Grid>
+   
+// <Box mt="250px">
+// <Footer/>
+// </Box>
+   
+
+//         </>
+//     )
+// }
+// export default Login;
